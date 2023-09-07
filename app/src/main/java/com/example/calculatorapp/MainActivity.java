dae728c563b843ba99ad96624a32eacfc08cb61b
AndyLFHS2025 <93545706+AndyLFHS2025@users.noreply.github.com>
2023-09-07 12:55:13 -0500

    finished xml, need to code java functionality still

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView output = findViewById(R.id.output);
+    private boolean equalPressed = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    public void numPressed(View view) {
+        int id = view.getId();
+        output.setText(id + "test");
+    }
+
+//    public String expPressed(View view) {
+//
+//    }
+
+
+    public void clear() {
+        output.setText("");
+    }
+
+
 }
