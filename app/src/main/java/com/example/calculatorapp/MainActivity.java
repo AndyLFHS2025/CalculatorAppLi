d3fee1d25116a452b69627e4a27c227ebe616e2c
AndyLFHS2025 <93545706+AndyLFHS2025@users.noreply.github.com>
2023-09-11 21:56:19 -0500

    got calculator functions to work properly and give accurate answers, still need to workout special numbers like pie and e

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -10,71 +10,114 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.calculator.R;
+
 public class MainActivity extends AppCompatActivity {
     private TextView answerText;
+    private String param1;
+    private String param2;
+    private String expression;
+    private boolean answerInText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         answerText = findViewById(R.id.output);
-        String firstNum = "";
-        String secondNum = "";
-        String expression = "";
-        String output = "";
+        param1 = "";
+        param2 = "";
+        expression = "";
+        answerInText = false;
     }
 
-    public String expressionClicked(View view) {
-        Button button = (Button)view;
-        String expression = button.getText().toString();
-        answerText.setText(answerText.getText() + " " + expression + " ");
-        return expression;
+    public void expressionClicked(View view) {
+        Button button = (Button) view;
+        String exp = button.getText().toString();
+
+        if (exp.equals(".")) {
+            answerText.setText(answerText.getText() + exp);
+        } else {
+            answerText.setText(answerText.getText() + " " + exp + " ");
+
+            expression = expression;
+        }
     }
 
-    public Double numClicked(View view) {
+    public void numClicked(View view) {
+
+        if (answerInText) {
+            clearAnswerText(view);
+            answerInText = false;
+        }
 
         Button button = (Button)view;
         String number = button.getText().toString();
         answerText.setText(answerText.getText().toString() + number);
-
-        if (number.equals("𝝿")) {
-            number = "3.14";
-        }
-
-        if (number.equals("e")) {
-            number = "2.72";
-        }
-        return Double.parseDouble(number);
     }
 
-    public void calculate(String param1, String param2, String exp) {
+    public void calculate(View view) {
+        char[] expressions = {'+', '-', 'X', '/', '%'};
         Double result;
-        Double number1 = Double.parseDouble(param1);
-        Double number2 = Double.parseDouble(param2);
+        String input = answerText.getText().toString();
+        int expressionIndex = -1;
 
-
-        if (exp.equals("/")) {
-            result = number1/number2;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = 0; j < expressions.length; j++) {
+                if (input.charAt(i) == expressions[j]) {
+                    expressionIndex = i;
+                    expression = Character.toString(expressions[j]);
+                }
+            }
         }
 
-        else if (exp.equals("X")) {
-            result = number1 * number2;
-        }
-
-        else if (exp.equals("+")) {
-            result = number1 + number2;
-        }
-
-        else if (exp.equals("-")) {
-            result = number1 - number2;
+        if (expressionIndex == -1) {
+            answerText.setText("Syntax error");
         }
 
         else {
-            result = number1 % number2;
+
+            param1 = input.substring(0, expressionIndex - 1);
+            param2 = input.substring(expressionIndex + 1);
+
+            if (param2.equals("") || param1.equals("")) {
+                answerText.setText("Syntax error");
+            }
+
+            else {
+
+                Double number1 = Double.parseDouble(param1);
+                Double number2 = Double.parseDouble(param2);
+
+                if (expression.equals("/")) {
+                    result = number1 / number2;
+                } else if (expression.equals("X")) {
+                    result = number1 * number2;
+                } else if (expression.equals("+")) {
+                    result = number1 + number2;
+                } else if (expression.equals("-")) {
+                    result = number1 - number2;
+                } else {
+                    result = number1 % number2;
+                }
+
+                result = Math.round(result * 1000.0) / 1000.0;
+
+                answerText.setText(result.toString());
+
+                resetParameters(view);
+            }
         }
 
+        answerInText = true;
     }
 
+    public void clearAnswerText(View view) {
+        answerText.setText("");
+    }
 
-
+    public void resetParameters(View view) {
+        param1 = "";
+        param2 = "";
+        expression = "";
+    }
 }
